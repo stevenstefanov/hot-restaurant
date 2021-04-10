528bc8f286f82bc0f6e952c797fd3c87ebb86c67
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-10 13:21:11 -0400

    Add waitlist table functionality

--- data-routes/reservation-data.js
+++ data-routes/reservation-data.js
@@ -1,10 +1,10 @@
 const waitlist = [
-  {
-    name: 'John Doe',
-    email: 'hungryjohn@gmail.com',
-    id: '01',
-    phone: '555-555-5555',
-  },
+  // {
+  // name: 'John Doe',
+  // email: 'hungryjohn@gmail.com',
+  // id: '01',
+  // phone: '555-555-5555',
+  // },
 ];
 
 module.exports = waitlist;
