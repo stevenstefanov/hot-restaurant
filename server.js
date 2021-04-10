b6ec89beb312cf25ea925627f3c527264624bff3
yondav <yonidavid.abr@gmail.com>
2021-04-10 12:41:42 -0400

    form works for post method

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Dependencies
-const express = require("express");
-const path = require("path");
-const tables = require("./data-routes/table-data");
-const waitlist = require("./data-routes/reservation-data");
+const express = require('express');
+const path = require('path');
+const tables = require('./data-routes/table-data');
+const waitlist = require('./data-routes/reservation-data');
 
 // Sets up the Express App
 const app = express();
@@ -12,30 +12,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Routes
 // Basic route that sends the user first to the AJAX Page
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
-);
-app.get("/tables", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/tables.html"))
-);
-app.get("/reserve", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/reserve.html"))
-);
-app.get("/api/tables", (req, res) => res.json(tables));
-app.get("/api/waitlist", (req, res) => res.json(waitlist));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, './public/tables.html')));
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, './public/reserve.html')));
+app.get('/api/tables', (req, res) => res.json(tables));
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
 
-app.post("/api/tables", (req, res) => {
+app.post('/api/tables', (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   const newRes = req.body;
+  console.log(req.body);
   if (tables.length < 5) {
     tables.push(newRes);
-    res.json("Reservation confirmed, we have you at a table");
+    res.json('Reservation confirmed, we have you at a table');
   } else {
     waitlist.push(newRes);
-    res.json(
-      "Reservation received, no tables available, but you are on the waiting list"
-    );
+    res.json('Reservation received, no tables available, but you are on the waiting list');
   }
 });
 // Starts the server to begin listening
