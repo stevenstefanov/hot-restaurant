efd0f0f54040dfecfc5861f2354a0a321084e8cf
inknsharps <nchen25@gmail.com>
2021-04-10 10:53:07 -0400

    update index and routing

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.json());
 // Routes
 // Basic route that sends the user first to the AJAX Page
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get("/tables", (req, res) => res.sendFile(path.join(__dirname, "./public/tables.html")));
+app.get("/reserve", (req, res) => res.sendFile(path.join(__dirname, "./public/reserve.html")));
 app.get("/api/tables", (req, res) => res.json(tables));
 app.get("/api/waitlist", (req, res) => res.json(waitlist));
 // Starts the server to begin listening
