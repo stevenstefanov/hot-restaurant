98ab39a79d00ad506ef8e2e5dc5d6988246d773b
inknsharps <nchen25@gmail.com>
2021-04-10 10:35:29 -0400

    fix pathing for get index.html request

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Routes
 // Basic route that sends the user first to the AJAX Page
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 app.get("/api/tables", (req, res) => res.json(tables));
 app.get("/api/waitlist", (req, res) => res.json(waitlist));
 // Starts the server to begin listening
